1bb45a9e3fdf22aaa45331da1c487ff4a2e25951
Johann Schley <65346790+oujonny@users.noreply.github.com>
2022-12-04 15:53:33 +0100

    update referenzen (#10)

--- themes/business/static/js/script.js
+++ themes/business/static/js/script.js
@@ -1,3 +1,4 @@
+// close nav on link click
 const navLinks = document.querySelectorAll('.nav-item')
 const menuToggle = document.getElementById('navbarSupportedContent')
 const bsCollapse = new bootstrap.Collapse(menuToggle, {
@@ -6,3 +7,8 @@ const bsCollapse = new bootstrap.Collapse(menuToggle, {
 navLinks.forEach((l) => {
     l.addEventListener('click', () => { bsCollapse.toggle() })
 })
+
+// randomize referenzen
+const caourselItems = document.querySelectorAll('.carousel-item');
+var item = caourselItems[Math.floor(Math.random()*caourselItems.length)];
+item.classList.add('active')
